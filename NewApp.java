19e04bca99203b563b8b1cf650bd4b15e5c2999f
vemulasathwik72 <vemulasathwik72@gmail.com>
2024-09-26 23:13:02 +0530

    newapp.java file aded

---
+++ NewApp.java
@@ -0,0 +1,22 @@
+public class MyClass {
+
+    // Public field
+    public int publicField = 10;
+
+    // Public method
+    public void publicMethod() {
+        System.out.println("This is a public method.");
+    }
+
+    // Main method to run the program
+    public static void main(String[] args) {
+        // Creating an object of MyClass
+        MyClass myObject = new MyClass();
+
+        // Accessing the public field
+        System.out.println("Public Field: " + myObject.publicField);
+
+        // Calling the public method
+        myObject.publicMethod();
+    }
+}
