7f9696b3c3d12517068b70c9c2f932ac4f969411
vemulasathwik72 <vemulasathwik72@gmail.com>
2024-09-27 00:18:09 +0530

    feature2.2 changes by sathwik

--- NewApp.java
+++ NewApp.java
@@ -7,6 +7,24 @@ public class MyClass {
     public void publicMethod() {
         System.out.println("This is a public method.");
     }
+#feature2.2 changes by sathwik
+public class Main {
+
+  // method defined
+  private static int getSquare(int x){
+    return x * x;
+  }
+
+  public static void main(String[] args) {
+    for (int i = 1; i <= 5; i++) {
+
+      // method call
+      int result = getSquare(i);
+      System.out.println("Square of " + i + " is: " + result);
+    }
+  }
+}
+#end of feature2.2
 
     // Main method to run the program
     public static void main(String[] args) {
